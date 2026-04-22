ea38f37856be99095d4c7434d588664349a4ab1d
Nos_ave <nos@ik.me>
2026-04-22 11:24:27 +0200

    [DEV] Add script distance

--- Assets/_Project/Scripts/ChangeColorIfNearby.cs
+++ Assets/_Project/Scripts/ChangeColorIfNearby.cs
@@ -1,43 +1,45 @@
-using System;
 using UnityEngine;
 
-public class ChangeColorIfNearby : MonoBehaviour
+namespace _Project.Scripts
 {
-    [SerializeField, Tooltip("Distance at which we change the color")] private float _colorSwapThreshold = 1.4f;
-    [SerializeField, Tooltip("Default Material of this cube")] private Material _defaultMaterial;
-    [SerializeField] private Material _nearbyMaterial;
-    [SerializeField] private MeshRenderer _renderer;
-    [SerializeField] private TMPro.TMP_Text _text;
-    [SerializeField] private Transform _textContainer;
+    public class ChangeColorIfNearby : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Distance at which we change the color")] private float _colorSwapThreshold = 1.4f;
+        [SerializeField, Tooltip("Default Material of this cube")] private Material _defaultMaterial;
+        [SerializeField] private Material _nearbyMaterial;
+        [SerializeField] private MeshRenderer _renderer;
+        [SerializeField] private TMPro.TMP_Text _text;
+        [SerializeField] private Transform _textContainer;
     
-    private Camera _camera;
+        private Camera _camera;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
-    {
-        if(_renderer == null) Debug.LogWarning("No MeshRenderer found!");
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Awake()
+        {
+            if(_renderer == null) Debug.LogWarning("No MeshRenderer found!");
+        }
 
-    private void Start()
-    {
-        _camera = Camera.main;
-    }
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = _text.transform.position - _camera.transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction);
+        // Update is called once per frame
+        private void Update()
+        {
+            Vector3 direction = _text.transform.position - _camera.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
         
-        Vector3 distanceTestVector =  transform.position - _camera.transform.position;
+            Vector3 distanceTestVector =  transform.position - _camera.transform.position;
         
-        float distance =  distanceTestVector.magnitude;
+            float distance =  distanceTestVector.magnitude;
 
-        if (distance > _colorSwapThreshold) _renderer.material = _defaultMaterial;
-        else _renderer.material = _nearbyMaterial;
-        _text.SetText(distance.ToString("F2"));
-        _text.transform.rotation = rotation;
+            if (distance > _colorSwapThreshold) _renderer.material = _defaultMaterial;
+            else _renderer.material = _nearbyMaterial;
+            _text.SetText(distance.ToString("F2"));
+            _text.transform.rotation = rotation;
         
+        }
     }
 }
